fc89b67bda5c9793ce22bff7ca464edcb6713fc2
Banri Yasui <sinsekainokami1773@ezweb.ne.jp>
2020-08-05 20:07:12 +0900

    HoT: Initial revision

---
+++ hello.py
@@ -0,0 +1,16 @@
+import random
+
+string=""
+countH=0
+countT=0
+print("Tossing a coin...")
+for i in range(3):
+    x=random.randrange(2)
+    if x==0:
+        countH=countH+1
+        string="Heads"
+    else:
+        countT=countT+1
+        string="Tails"
+    print("Round "+str(i+1)+": "+string)
+print("Heads: "+str(countH)+", Tails: "+str(countT))
